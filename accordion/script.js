1faaf2d86966dc212ee27eb91ebc1fb4e39818fc
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-10-13 19:00:46 +0530

    add transition to maxHeight and

--- accordion/script.js
+++ accordion/script.js
@@ -5,7 +5,12 @@ accordions.forEach((accordion) => {
   const answer = accordion.querySelector(".answer");
 
   accordion.addEventListener("click", () => {
-    icon.classList.toggle("active");
-    answer.classList.toggle("active");
+    if (icon.classList.contains("active")) {
+      icon.classList.remove("active");
+      answer.style.maxHeight = null;
+    } else {
+      icon.classList.add("active");
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
   });
 });
